508582b6be404fcfb9d2a3f4ce3879cc620f6963
Bartosz Klinowski <bartoszklinowski@gmail.com>
2022-05-29 03:42:02 +0200

    styling header

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import ejs from "ejs"
 
 const app = express()
 
@@ -7,6 +8,8 @@ dotenv.config();
 app.use(express.json());
 app.use(express.static('public'))
 
+import routes from "./routes/routes.js"
+app.use(routes)
 
 app.listen(process.env.PORT || 3000, function () {
     console.log(`Server listening for connection requests on socket localhost: ${process.env.PORT || 3000}`);
--- routes/routes.js
+++ routes/routes.js
@@ -2,3 +2,8 @@ import router from "express"
 
 const Router = router()
 
+Router.get("/", (req, res, next) => {
+    res.render("main.ejs")
+})
+
+export default Router
