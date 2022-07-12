739f8ae7f570bcd740c5728a25fb02bb5ed07fda
Bartosz Klinowski <bartoszklinowski@gmail.com>
2022-07-13 01:24:17 +0200

    few pages added

--- routes/routes.js
+++ routes/routes.js
@@ -19,4 +19,32 @@ Router.get("/services", (req, res, next) => {
   res.render("services.ejs")
 })
 
+Router.get("/e-commerce", (req, res, next) => {
+  res.render("e-commerce.ejs")
+})
+
+Router.get("/websites", (req, res, next) => {
+  res.render("websites.ejs")
+})
+
+Router.get("/mobiles", (req, res, next) => {
+  res.render("mobiles.ejs")
+})
+
+Router.get("/feedback", (req, res, next) => {
+  res.render("feedback.ejs")
+})
+
+Router.get("/career", (req, res, next) => {
+  res.render("career.ejs")
+})
+
+Router.get("/contact", (req, res, next) => {
+  res.render("contact.ejs")
+})
+
+Router.get("/about-us", (req, res, next) => {
+  res.render("about-us.ejs")
+})
+
 export default Router;
