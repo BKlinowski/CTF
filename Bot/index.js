f8c3a9bfa41c88d2c572713eae35b4a40e57916a
Bartosz Klinowski <bartoszklinowski@gmail.com>
2025-12-05 01:21:26 +0100

    Add initial Kubernetes cluster configuration

--- Bot/index.js
+++ Bot/index.js
@@ -2,7 +2,7 @@ import puppeteer from "puppeteer";
 
 (async () => {
   const browser = await puppeteer.launch({
-    executablePath: "/usr/bin/google-chrome",
+    executablePath: "/usr/bin/chromium-browser",
     args: ["--no-sandbox"],
   });
   const [page] = await browser.pages();
