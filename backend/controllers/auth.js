f926d38c3c0b1c168955834d2a2d4e6a4ed8adae
Bartosz Klinowski <bartoszklinowski@gmail.com>
2025-12-09 02:05:09 +0100

    add auth logging

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -5,7 +5,7 @@ import { v4 as uuidv4 } from "uuid";
 
 export const postLogin = async (req, res) => {
   const { email, password } = req.body;
-  console.log(email)
+  console.log("email: ", email)
   const [userExists] = await db.queryRows(
     "select exists(SELECT FROM users WHERE email = $1);",
     [email]
