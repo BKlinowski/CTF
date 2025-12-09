e3eada861a06ddaef31f8fdfd16d01c441ee2982
Bartosz Klinowski <bartoszklinowski@gmail.com>
2025-12-09 09:30:33 +0100

    Refactor build action

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -5,8 +5,6 @@ import { v4 as uuidv4 } from "uuid";
 
 export const postLogin = async (req, res) => {
   const { email, password } = req.body;
-  console.log("email: ", email)
-  console.log("test")
   const [userExists] = await db.queryRows(
     "select exists(SELECT FROM users WHERE email = $1);",
     [email]
